97e6e9a9103d8416fa160f5a26a6c60c3a0858de
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-10-20 10:07:55 +0700

    Initial Commit

---
+++ SwiftUI-Crypto/SwiftUI_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoApp.swift
+//  SwiftUI-Crypto
+//
+//  Created by Hadid Hardiansyah Saputra on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
