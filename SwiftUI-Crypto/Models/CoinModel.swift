98eeb13194674afb8bbbde62fba5fe55cea31a8b
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-10-22 21:50:29 +0700

    Download coins using a View Model and Data Service class

--- SwiftUI-Crypto/Models/CoinModel.swift
+++ SwiftUI-Crypto/Models/CoinModel.swift
@@ -71,7 +71,7 @@ struct CoinModel: Identifiable, Codable {
     
     enum CodingKeys: String, CodingKey {
         case id, symbol, name, image
-        case currentPrice = "current-price"
+        case currentPrice = "current_price"
         case marketCap = "market_cap"
         case marketCapRank = "market_cap_rank"
         case fullyDilutedValuation = "fully_diluted_valuation"
---
+++ SwiftUI-Crypto/Services/CoinDataService.swift
@@ -0,0 +1,62 @@
+//
+//  CoinDataService.swift
+//  SwiftUI-Crypto
+//
+//  Created by Hadid Hardiansyah Saputra on 21/10/24.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=true&price_change_percentage=24h") else { return }
+        
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+          "accept": "application/json",
+          "x-cg-demo-api-key": "\(apiKey)"
+        ]
+        
+        
+        
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: request)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink { (completion) in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            }
+
+    }
+    
+}
