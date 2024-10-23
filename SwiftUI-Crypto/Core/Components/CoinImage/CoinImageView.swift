d08bccfc5c7e02a7042125f1280c6d7b1294dabc
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-10-24 06:58:31 +0700

    Downloading Coin Images using Combine, MVVM, and a Networking Layer

---
+++ SwiftUI-Crypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,40 @@
+//
+//  CoinImageView.swift
+//  SwiftUI-Crypto
+//
+//  Created by Hadid Hardiansyah Saputra on 24/10/24.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundColor(.theme.secondaryText)
+            }
+        }
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView(coin: dev.coin)
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
