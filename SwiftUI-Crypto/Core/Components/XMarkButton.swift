f7252c33686ed91b1948e7d84651ea079af9d69f
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-10-26 12:07:21 +0700

    Create a view to manager current user's portfolio

---
+++ SwiftUI-Crypto/Core/Components/XMarkButton.swift
@@ -0,0 +1,28 @@
+//
+//  XMarkButton.swift
+//  SwiftUI-Crypto
+//
+//  Created by Hadid Hardiansyah Saputra on 26/10/24.
+//
+
+import SwiftUI
+
+struct XMarkButton: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        Button(action: {
+            dismiss()
+        }, label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        })
+    }
+}
+
+struct XMarkButton_Previews: PreviewProvider {
+    static var previews: some View {
+        XMarkButton()
+    }
+}
--- SwiftUI-Crypto/SwiftUI_CryptoApp.swift
+++ SwiftUI-Crypto/SwiftUI_CryptoApp.swift
@@ -12,6 +12,11 @@ struct SwiftUI_CryptoApp: App {
     
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
